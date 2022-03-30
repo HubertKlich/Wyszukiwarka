d426e0ca405072e96821d86304cd4c69feafa6cc
HubertKlich <hubert.klich@wp.pl>
2022-03-30 22:44:54 +0200

    Add files via upload

--- Run.bat
+++ Run.bat
@@ -1,11 +1,11 @@
 @ECHO OFF
-"Odpalanie wyszukiwarki:"
+echo Odpalanie wyszukiwarki:
 :loop
-set argument=""
-echo "Jakie slowo chcesz wyszukac?"
+set argument=
+echo Jakie slowo chcesz wyszukac?
 set /P argument=
-if %argument% EQU "" goto exit
+if "%argument%" EQU "" goto exit
 python3 Wyszukiwarka.py %argument%
-if %argument% NEQ "" goto loop
+if "%argument%" NEQ "" goto loop
 :exit
 PAUSE
