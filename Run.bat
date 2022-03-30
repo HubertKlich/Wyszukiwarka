d2212b43693a01bc44f7661200e3f2a94573098c
HubertKlich <hubert.klich@wp.pl>
2022-03-30 22:49:28 +0200

    Add files via upload

--- Run.bat
+++ Run.bat
@@ -2,8 +2,10 @@
 echo Odpalanie wyszukiwarki:
 :loop
 set argument=
-echo Jakie slowo chcesz wyszukac?
+echo.
+echo Jakie slowo chcesz wyszukac? (Jezeli chcesz wyjsc kliknij ENTER)
 set /P argument=
+cls
 if "%argument%" EQU "" goto exit
 python3 Wyszukiwarka.py %argument%
 if "%argument%" NEQ "" goto loop
